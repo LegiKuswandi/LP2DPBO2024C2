5b671ec73e95d3025378d7c0236feba48fc2ad87
LegiKuswandi <147142081+LegiKuswandi@users.noreply.github.com>
2024-02-20 19:02:36 +0700

    Update Shirt.java

--- Java/Program/Shirt.java
+++ Java/Program/Shirt.java
@@ -6,21 +6,6 @@ public class Shirt extends Clothing {
         this.color = "";
         this.sleeve_type = "";
     }
-    //Konstruktor parameter untuk shirt
-    public Shirt(String idProduct, String name, String brand, String price, String size, String material, String gender, String color, String sleeve_type){
-        this.color = color;
-        this.sleeve_type = sleeve_type;
-        //Pemanggilan beberapa method set dari ortu
-        setIdProduct(idProduct);
-        setName(name);
-        setBrand(brand); 
-        setPrice(price);
-        setSize(size);
-        //Pemanggilan beberapa method set dari ortu
-        setMaterial(material);
-        setGender(gender);
-        setColor(color);
-    }
     //Method set untuk sleeve type
     public void setSleeve_type(String sleeve_type) {
         this.sleeve_type = sleeve_type;
